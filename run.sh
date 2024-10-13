8a562bee508795390474f3fa8a23ba77e92e7ca3
shin <fatshin@gmail.com>
2024-10-13 18:35:04 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # デフォルトのパスを設定
-DEFAULT_PATH="/output_json/service_catalog.json"
+DEFAULT_PATH="./output_json/service_catalog.json"
 
 # コマンドライン引数からパスを取得（指定されていない場合はデフォルトを使用）
 SERVICE_CATALOG_PATH=${1:-$DEFAULT_PATH}
 
-# Windowsパスをdocker-compose用に変換
-if [[ "$OSTYPE" == "msys"* ]]; then
-    SERVICE_CATALOG_PATH=$(echo $SERVICE_CATALOG_PATH | sed -e 's/\\/\//g' -e 's/://')
-    SERVICE_CATALOG_PATH="/${SERVICE_CATALOG_PATH}"
-fi
+# 絶対パスに変換
+SERVICE_CATALOG_PATH=$(realpath "$SERVICE_CATALOG_PATH")
+
+# Dockerコンテナ内のパスに変換
+DOCKER_PATH="/work/output_json/service_catalog.json"
 
 # 環境変数をエクスポート
-export SERVICE_CATALOG_PATH
+export HOST_SERVICE_CATALOG_PATH=$SERVICE_CATALOG_PATH
+export DOCKER_SERVICE_CATALOG_PATH=$DOCKER_PATH
 
 # docker-composeコマンドを実行
 docker compose up --build
